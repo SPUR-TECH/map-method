78afb1fd3b43017a65cf3e21076c02a5671ea3f2
Scott Atkinson <scott.atkin79@gmail.com>
2022-03-07 21:52:32 +0000

    map method

--- map.js
+++ map.js
@@ -1,19 +1,33 @@
-  
+
 /**
  * To run this file in Gitpod, use the 
  * command node map.js in the terminal
  */
 
 // Using a for loop
+let nums = [1, 2, 3, 4, 5];
+let results = [];
+for (let num of nums) {
+  results.push(num * 2);
+}
 
+console.log(results);
 
 // Using map()
+const multByTwo = function (num) {
+  return num * 2;
+}
+const mapResults = nums.map(multByTwo);
+console.log(mapResults);
 
 
 // Simplified w/ map()
+const simplified = nums.map(function (num) { return num * 2 });
+console.log(simplified);
 
-
-// Simplfied w/ map() + arrow function
+// Simplified w/ map() + arrow function
+const arrow = nums.map(num => num * 2);
+console.log(arrow);
 
 
 // With objects:
@@ -37,3 +51,6 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentsWithIds = students.map(student => [student.name, student.id]);
+console.log(studentsWithIds);
